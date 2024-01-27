5c02eda96b672a6ce5d41439b95b6b6f01f5dbee
indeaguy <davide.mcguigan@gmail.com>
2024-01-27 18:47:52 -0400

    Trying to get github action to accept RSA in deploy script.

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -av --exclude='.git*' -e "ssh -p ${SSH_PORT}" ./ ${SSH_USER}@${SSH_HOST}:/${SSH_PATH}
+rsync -av --exclude='.git*' -e "ssh -o HostKeyAlgorithms=+ssh-rsa -p ${SSH_PORT}" ./ ${SSH_USER}@${SSH_HOST}:/${SSH_PATH}
